7e6f120e0355969037bc7fb240a5fd931c6db4e8
Imad Saddik <simad3647@gmail.com>
2024-12-10 20:41:15 +0100

    Added prime icons as dependency

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -4,6 +4,7 @@ import router from "./router";
 import PrimeVue from "primevue/config";
 import Tooltip from 'primevue/tooltip';
 import Aura from "@primevue/themes/aura"
+import 'primeicons/primeicons.css'
 
 const app = createApp(App);
 app.use(router);
