81f8d8356e15d9376c189523169992adf2b618d7
Imad Saddik <simad3647@gmail.com>
2024-12-10 08:31:47 +0100

    Added the side panel

---
+++ frontend/src/components/SidePanel.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="container">Hi, I'm a side panel!</div>
+</template>
+
+<script>
+export default {
+  name: "SidePanel",
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style scoped>
+.container {
+  position: absolute;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  width: 20rem;
+  background-color: rgba(255, 255, 255, 0.95);
+  border-left: 1px solid #adadad;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.3);
+  z-index: 1000;
+  overflow-y: auto;
+  padding: 1rem;
+}
+</style>
--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,14 +1,17 @@
 <template>
   <LeafletMap />
+  <SidePanel />
 </template>
 
 <script>
 import LeafletMap from '@/components/LeafletMap.vue';
+import SidePanel from '@/components/SidePanel.vue';
 
 export default {
   name: "HomeView",
   components: {
     LeafletMap,
+    SidePanel
   },
   data() {
     return {
