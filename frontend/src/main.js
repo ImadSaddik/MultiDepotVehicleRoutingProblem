d670b659cd67fb72776b1d7562438ea51b27ee75
Imad Saddik <simad3647@gmail.com>
2024-12-13 22:08:36 +0100

    Configured axios

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -5,12 +5,14 @@ import router from "./router";
 import PrimeVue from "primevue/config";
 import Tooltip from 'primevue/tooltip';
 import Aura from "@primevue/themes/aura"
-import 'primeicons/primeicons.css'
 import ToastService from 'primevue/toastservice';
+import axios from "axios";
+import 'primeicons/primeicons.css'
 
+axios.defaults.baseURL = 'http://localhost:8000'
 const app = createApp(App);
 app.use(createPinia())
-app.use(router);
+app.use(router, axios);
 app.use(PrimeVue, {
     theme: {
         preset: Aura,
