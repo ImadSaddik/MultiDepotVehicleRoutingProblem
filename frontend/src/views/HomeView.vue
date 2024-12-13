0cbca92502cde09f240ea351bf67635dd0dcc18c
Imad Saddik <simad3647@gmail.com>
2024-12-13 20:51:05 +0100

    Added Close/Open side panel button

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -6,22 +6,32 @@
     :showEmployees
     :showBuses
   />
-  <SidePanel 
-    @sliced-data="handleSlicedData"
-    @employee-toggle-switch-change="handleEmployeeToggleSwitchChange"
-    @bus-toggle-switch-change="handleBusToggleSwitchChange"
+  <Button 
+    :icon="isSidePanelOpen ? 'pi pi-chevron-right' : 'pi pi-chevron-left'" 
+    class="toggle-button" 
+    @click="toggleSidePanelVisibility" 
   />
+  <transition name="slide-panel" mode="out-in">
+    <SidePanel
+      v-if="isSidePanelOpen"
+      @sliced-data="handleSlicedData"
+      @employee-toggle-switch-change="handleEmployeeToggleSwitchChange"
+      @bus-toggle-switch-change="handleBusToggleSwitchChange"
+    />
+  </transition>
 </template>
 
 <script>
-import LeafletMap from '@/components/LeafletMap.vue';
+import Button from 'primevue/button';
 import SidePanel from '@/components/SidePanel.vue';
+import LeafletMap from '@/components/LeafletMap.vue';
 
 export default {
   name: "HomeView",
   components: {
-    LeafletMap,
-    SidePanel
+    Button,
+    SidePanel,
+    LeafletMap
   },
   data() {
     return {
@@ -29,7 +39,8 @@ export default {
       busData: [],
       companyData: {},
       showEmployees: true,
-      showBuses: true
+      showBuses: true,
+      isSidePanelOpen: true
     };
   },
   methods: {
@@ -43,10 +54,30 @@ export default {
     },
     handleBusToggleSwitchChange(value) {
       this.showBuses = value;
+    },
+    toggleSidePanelVisibility() {
+      this.isSidePanelOpen = !this.isSidePanelOpen;
     }
   }
 };
 </script>
 
 <style scoped>
+.toggle-button {
+  position: absolute;
+  top: 1rem;
+  right: 1rem;
+  z-index: 2000;
+}
+
+.slide-panel-enter-active,
+.slide-panel-leave-active {
+  transition: transform 0.3s ease-in-out;
+}
+.slide-panel-enter-from {
+  transform: translateX(100%);
+}
+.slide-panel-leave-to {
+  transform: translateX(100%);
+}
 </style>
