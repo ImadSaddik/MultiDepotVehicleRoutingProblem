c237f63eeea193d8b9942f977df903ac7c78c63c
Imad Saddik <simad3647@gmail.com>
2024-12-15 15:52:51 +0100

    Added buttons to cycle through bus-employees clusters

--- frontend/src/components/SidePanelStep4.vue
+++ frontend/src/components/SidePanelStep4.vue
@@ -1,11 +1,31 @@
 <template>
   <div class="step-container">
+    <Toast position="bottom-left" />
+
     <h1>Routing solver</h1>
     <h2>Results visualization</h2>
 
     <p>
       The results are displayed in the map. You can cycle through each bus to see the optimized route.
     </p>
+
+    <div class="cluster-selection-buttons">
+      <Button 
+        :disabled="selectedCluster === 0"
+        icon="pi pi-chevron-left"
+        class="toggle-button"
+        @click="showPreviousCluster" 
+      />
+
+      <p>Bus N° {{ selectedCluster }}</p>
+
+      <Button 
+        :disabled="selectedCluster === store.optimizedData.length - 1"
+        icon="pi pi-chevron-right"
+        class="toggle-button" 
+        @click="showNextCluster" 
+      />
+    </div>
 
     <Button
       label="Back"
@@ -19,11 +39,13 @@
 <script>
 import { dataStore } from "@/store/dataStore";
 
+import Toast from "primevue/toast";
 import Button from "primevue/button";
 
 export default {
   name: "SidePanelStep4",
   components: {
+    Toast,
     Button,
   },
   setup() {
@@ -32,17 +54,50 @@ export default {
   },
   data() {
     return {
+      selectedCluster: 0,
     };
   },
   methods: {
     goToPreviousStep() {
       this.$emit("previous-step");
     },
+    showNextCluster() {
+      if (this.selectedCluster === this.store.optimizedData.length - 1) {
+        this.$toast.add({
+          severity: "error",
+          summary: "Error",
+          detail: "You are already at the last cluster",
+          life: 5000,
+        });
+        return;
+      }
+
+      this.selectedCluster = this.selectedCluster + 1;
+    },
+    showPreviousCluster() {
+      if (this.selectedCluster === 0) {
+        this.$toast.add({
+          severity: "error",
+          summary: "Error",
+          detail: "You are already at the first cluster",
+          life: 5000,
+        });
+        return;
+      }
+
+      this.selectedCluster = this.selectedCluster - 1;
+    },
   },
 };
 </script>
 
 <style scoped>
+.cluster-selection-buttons {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
 .back-button {
   position: absolute;
   bottom: 1rem;
