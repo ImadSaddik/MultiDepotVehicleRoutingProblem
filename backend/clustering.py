7ffef8298c590459c610fdafc53eb04b4075bde1
Imad Saddik <simad3647@gmail.com>
2024-12-15 15:22:09 +0100

    Changed the clustering script

--- backend/clustering.py
+++ backend/clustering.py
@@ -1,34 +1,46 @@
 from typing import List
+from psycopg2.extensions import cursor
+
+from models import Node
+from utils import get_route_segments
 
 def assign_employees_to_buses(
-    buses_data: List[tuple],
-    employees_data: List[tuple],
-    routes: List[dict],
+    bus_nodes: List[Node],
+    employee_nodes: List[Node],
+    cursor: cursor,
     max_capacity: int = 18
 ) -> dict:
-    assignments = {i: [] for i in range(len(buses_data))}
-    unassigned_employees = set(range(len(employees_data)))
+    bus_ids = [node.id for node in bus_nodes]
+    employee_ids = [node.id for node in employee_nodes]
 
-    for bus_idx, bus_location in enumerate(buses_data):
+    assignments = {i: [] for i in bus_ids}
+    unassigned_employees = set(employee_ids)
+
+    for bus_node in bus_nodes:
         if not unassigned_employees:
             break
 
         distances = {}
-        for emp_idx in unassigned_employees:
-            key = tuple(
-                sorted((tuple(bus_location), tuple(employees_data[emp_idx]))))
-            if key in routes:
-                distances[emp_idx] = routes[key]['distance']
-            else:
-                distances[emp_idx] = float('inf')
+        bus_node_id = bus_node.id
+        for employee_node_id in unassigned_employees:
+            route_segment = get_route_segments(
+                source_node_id=bus_node_id,
+                destination_node_id=employee_node_id,
+                cursor=cursor
+            )
+            if not route_segment:
+                distances[employee_node_id] = float('inf')
+                continue
+                
+            distances[employee_node_id] = route_segment.distance
 
         sorted_employee_indices = sorted(distances, key=distances.get)
 
-        for emp_idx in sorted_employee_indices[:max_capacity]:
-            if len(assignments[bus_idx]) < max_capacity:
-                assignments[bus_idx].append(emp_idx)
+        for employee_node_id in sorted_employee_indices[:max_capacity]:
+            if len(assignments[bus_node_id]) < max_capacity:
+                assignments[bus_node_id].append(employee_node_id)
 
-        assigned = set(assignments[bus_idx])
+        assigned = set(assignments[bus_node_id])
         unassigned_employees -= assigned
 
     return assignments
