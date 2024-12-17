13408e72a20455bcfe55664b410ff5ed84403c40
Imad Saddik <simad3647@gmail.com>
2024-12-17 21:57:27 +0100

    Removed storing the route display mode data in the store

--- frontend/src/components/LeafletMap.vue
+++ frontend/src/components/LeafletMap.vue
@@ -86,7 +86,6 @@ export default {
     routeDisplay: {
       handler(newValue) {
         if (newValue) {
-          this.store.setRouteDisplayMode(newValue);
           this.updateRouteIfVisible();
         }
       },
